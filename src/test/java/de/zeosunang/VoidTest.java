40a9cc271f2473c64dd79955089bd5f1b68a2ed0
Nils Schweikhard <zeosunang@gmail.com>
2017-06-23 00:21:10 +0200

    Added Simple Test

---
+++ src/test/java/de/zeosunang/VoidTest.java
@@ -0,0 +1,19 @@
+package de.zeosunang;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Zeosunang on 23.06.2017.
+ */
+public class VoidTest {
+
+    @Test()
+    public void testVoid() {
+
+        Assert.assertEquals("void", "void");
+
+    }
+
+
+}
